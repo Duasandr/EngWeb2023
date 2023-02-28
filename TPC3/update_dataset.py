329c1f3cc471c4c39b2711b6dfe3ad371290e681
Duasandr <sandro.duarte.alt@gmail.com>
2023-02-28 22:15:24 +0000

    Created update_dataset.py.

---
+++ TPC3/update_dataset.py
@@ -0,0 +1,19 @@
+import json
+
+# Load the dataset
+data = json.load(open('./datasets/dataset-extra1.json'))
+
+x = 0
+for person in data['pessoas']:
+    # Add id field to each person
+    person['id'] = f"p{x}"
+    x += 1
+
+# Open backup file to save the updated dataset
+backupFile = open('./datasets/dataset-extra1-backup.json', 'w', encoding='utf-8')
+
+# Save the new dataset to a backup file
+# The ensure_ascii=False parameter is used to save the file in UTF-8
+json.dump(data, backupFile, ensure_ascii=False, indent=4)
+
+backupFile.close()
